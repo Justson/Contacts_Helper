976700b8b8f21f8460deeef37be33c59b135eace
Justson <xiaozhongcen@gmail.com>
2016-10-16 03:20:08 +0800

    #

--- src/com/qypt/just_syn_asis_version1_0/activity/ContactManageActivity.java
+++ src/com/qypt/just_syn_asis_version1_0/activity/ContactManageActivity.java
@@ -22,7 +22,7 @@ import com.qypt.just_syn_asis_version1_0.bitmapUtils.BitmapUtils;
 /**
  * 
  * @author Administrator justson
- *	
+ *	c
  */
 public class ContactManageActivity extends Activity implements OnLongClickListener, OnItemClickListener, OnClickListener {
 
