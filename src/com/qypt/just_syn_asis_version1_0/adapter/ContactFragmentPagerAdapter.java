9919896151d58c6c3695b3fcbc68a39e91ae700d
Justson <xiaozhongcen@gmail.com>
2016-11-02 21:43:26 +0800

    #

--- src/com/qypt/just_syn_asis_version1_0/adapter/ContactFragmentPagerAdapter.java
+++ src/com/qypt/just_syn_asis_version1_0/adapter/ContactFragmentPagerAdapter.java
@@ -8,7 +8,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 /**
  * 
  * @author Administrator justson
- *
+ * 
  */
 public class ContactFragmentPagerAdapter extends FragmentPagerAdapter {
 
