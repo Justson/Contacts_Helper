825091b86629bd214daf84d71300bf3765f7df19
Justson <xiaozhongcen@gmail.com>
2016-11-02 21:42:23 +0800

    #

--- src/com/qypt/just_syn_asis_version1_0/adapter/GridViewBaseAdapter.java
+++ src/com/qypt/just_syn_asis_version1_0/adapter/GridViewBaseAdapter.java
@@ -59,7 +59,7 @@ public abstract class GridViewBaseAdapter<T> extends BaseAdapter {
 		v.startAnimation(view_in_animation);
 	}
 	
-	@Override//大小 i
+	@Override//大小 
 	public int getCount() {
 		// TODO Auto-generated method stub
 		return list.size();
